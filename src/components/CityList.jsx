317e0fca542f6ae245cfa58ab66ae56fdfc0c021
k906506 <k906506@naver.com>
2021-05-19 15:46:32 +0900

    구글 날씨검색으로 링크 연결

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,18 +1,25 @@
 function CityList(props) {
-    // const cities = props.cities;
-    const { cities } = props;
-  
-    return (
-      <div>
-        <h1>CityList</h1>
-  
-        <ul>
-          {cities.map((item, index) => {
-            return <li key={index}>{item}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-  
-  export default CityList;
+  // const cities = props.cities;
+  const { cities } = props;
+
+  return (
+    <div>
+      <h1>CityList</h1>
+
+      <ul>
+        {cities.map((item, index) => {
+          return <a
+            className="weather-element"
+            href={"https://www.google.com/search?q=" + item + " weather"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <li key={index}>{item}</li>
+          </a>
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default CityList;
